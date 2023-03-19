ae92d9096c0d6e18b56027ba2cf886471e1d9123
김세현 <101509164+repairedserver@users.noreply.github.com>
2023-03-19 13:00:41 +0900

    get post 기본 틀

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Union
 import uvicorn
+
+
+class Item(BaseModel):
+    prompt: str
+    W: Union[int, None] = None
+    H: Union[int, None] = None
+    steps: Union[int, None] = None
+    format: Union[str, None]
+    samples: Union[int, None] = None
+
 
 app = FastAPI()
 
-@app.get('/test')
+@app.get('/')
 def test():
     return {'test':356}
+
+@app.post('/test/')
+def post_test(item: Item):
+    return item
+
 
 if __name__ == '__main__':
     uvicorn.run("main:app", port=3000, reload=True)
