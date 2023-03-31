f140e5ca2ff2a37cb46175c10a742d53d845dafc
repairedserver2 <dnjfdid14@naver.com>
2023-03-31 17:09:46 +0900

    status code return

--- app/generate.py
+++ app/generate.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status
 from pydantic import BaseModel
 import subprocess as sub
 
@@ -13,7 +13,7 @@ class GenerateImage(BaseModel):
 
 prompt = APIRouter()
 
-# @prompt.post('/txt2img/keyword')
+# @prompt.post('/txt2img/keyword', status_code=status.HTTP_200_OK)
 # async def get_prompt_txt2img(image: GenerateImage):
 #     cmd = ['python', './optimizedSD/txt2img.py', '--prompt', f'{image.keyword}', '--W', 
 #            f'{image.W}', '--H', f'{image.H}', '--ddim_steps', f'{image.steps}', '--format', f'{image.format}', '--n_samples', f'{image.samples}']
@@ -21,17 +21,18 @@ prompt = APIRouter()
 #     return image.keyword
 
 
-@prompt.post('/img2img/keyword')
+@prompt.post('/img2img/keyword', status_code=status.HTTP_200_OK)
 async def get_prompt_img2img(image: GenerateImage):
     # TODO
     # mysql db e seo image gat go o gi
+    # and save image
     cmd = ['python', './optimizedSD/img2img.py', '--prompt', f'{image.keyword}', '--W', 
-            f'{image.W}', '--H', f'{image.H}', '--ddim_steps', f'{image.steps}', '--format', f'{image.format}', '--n_samples', f'{image.samples}', '--init-img', f'./input/pepe.png', '--turbo']
+            f'{image.W}', '--H', f'{image.H}', '--ddim_steps', f'{image.steps}', '--format', f'{image.format}', '--n_samples', f'{image.samples}', '--init-img', f'./input/input1.png', '--turbo']
     sub.run(cmd)
     return image.keyword
 
 
-@prompt.post('/inpaint/keyword')
+@prompt.post('/inpaint/keyword', status_code=status.HTTP_200_OK)
 async def get_prompt(image: GenerateImage):
     cmd = ['python', './optimizedSD/img2img.py', '--prompt', f'{image.keyword}', '--W', 
             f'{image.W}', '--H', f'{image.H}', '--ddim_steps', f'{image.steps}', '--format', f'{image.format}', '--n_samples', f'{image.samples}', '--init-img', f'path']
