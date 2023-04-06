684bfa4213f82a2591d9496c0f793cd4a3748595
repairedserver2 <dnjfdid14@naver.com>
2023-04-06 19:36:03 +0900

    Image request

--- app/generate.py
+++ app/generate.py
@@ -1,14 +1,17 @@
-from fastapi import APIRouter, status
+from fastapi import APIRouter, status, UploadFile, File, Form
 from pydantic import BaseModel
 import subprocess as sub
+import os
+import uuid
 
-class GenerateImage(BaseModel):
-    keyword: str
-    W: int
-    H: int
-    steps: int
-    format: str
-    samples: int
+
+# class GenerateImage(BaseModel):
+#     keyword: str
+#     W: int
+#     H: int
+#     steps: int
+#     format: str
+#     samples: int
 
 
 prompt = APIRouter()
@@ -22,20 +25,33 @@ prompt = APIRouter()
 
 
 @prompt.post('/img2img/keyword', status_code=status.HTTP_200_OK)
-async def get_prompt_img2img(image: GenerateImage):
-    # TODO
-    # mysql db e seo image gat go o gi
-    # and save image
-    cmd = ['python', './optimizedSD/img2img.py', '--prompt', f'{image.keyword}', '--W', 
-            f'{image.W}', '--H', f'{image.H}', '--ddim_steps', f'{image.steps}', '--format', f'{image.format}', '--n_samples', f'{image.samples}', '--init-img', f'./input/input1.png', '--turbo']
-    sub.run(cmd)
-    return image.keyword
+async def get_prompt_img2img(keyword: str = Form(), W: int = Form(), H: int = Form(),
+                            steps: int = Form(), format: str = Form(),
+                            samples: int = Form(), base_img: UploadFile = File()):
+
+    content = base_img.file.read()
+    filename = f"{str(uuid.uuid4())}.png"
+
+    with open(os.path.join('./input/img2img', filename), "w+b") as fp:
+        fp.write(content)
+
+    # cmd = ['python', './optimizedSD/img2img.py', '--prompt', f'{image.keyword}', '--W', 
+    #         f'{image.W}', '--H', f'{image.H}', '--ddim_steps', f'{image.steps}', '--format', 
+    #         f'{image.format}', '--n_samples', f'{image.samples}', '--init-img', f'./input/input1.png', '--turbo']
+    # sub.run(cmd)
+
+    return
 
 
 @prompt.post('/inpaint/keyword', status_code=status.HTTP_200_OK)
-async def get_prompt(image: GenerateImage):
-    cmd = ['python', './optimizedSD/img2img.py', '--prompt', f'{image.keyword}', '--W', 
-            f'{image.W}', '--H', f'{image.H}', '--ddim_steps', f'{image.steps}', '--format', f'{image.format}', '--n_samples', f'{image.samples}', '--init-img', f'path']
-    sub.run(cmd)
-    return image.keyword
+async def get_prompt(base_img: UploadFile = File()):
+    
+    content = base_img.file.read()
+    filename = f"{str(uuid.uuid4())}.png"
 
+    with open(os.path.join('./input/inpaint', filename), "w+b") as fp:
+        fp.write(content)
+
+    # cmd = ['python', './optimizedSD/inpaint.py']
+    # sub.run(cmd)
+    return
