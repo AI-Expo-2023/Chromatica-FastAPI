7dc58006100d49a09c4690d7fc111f718311cd7b
repairedserver2 <dnjfdid14@naver.com>
2023-04-06 19:36:54 +0900

    just update

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,6 @@
 from fastapi import APIRouter
 from app import generate
-from app import image
 
 api_router = APIRouter()
 
 api_router.include_router(generate.prompt)
-api_router.include_router(image.image_post)
