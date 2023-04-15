da064c44bd5fd30d99d64bb196b45f8b5d632d3a
repairedserver2 <dnjfdid14@naver.com>
2023-04-15 10:57:30 +0900

    image receive

--- app/generate.py
+++ app/generate.py
@@ -5,7 +5,7 @@ import subprocess as sub
 from googletrans import Translator
 from PIL import Image
 from io import BytesIO
-import os, time, base64
+import os, time, base64, time
 from optimizedSD import inpaint#, img2img
 from urllib import request
 
@@ -18,21 +18,28 @@ async def save_img2img_img(file):
     a = time.strftime("%Y-%m-%d-%H-%M-%S")
     filename = f"{str(a)}.png"
     path = './input/img2img'
+
     content_byte = str.encode(file)
     content = base64.b64decode(content_byte)
+
     with open(os.path.join(path, filename), "w+b") as fp:
         fp.write(content)
+
     return f'{path}/{filename}'
 
 
 async def save_base_img(file):
+    print('activate1')
     a = time.strftime("%Y-%m-%d-%H-%M-%S")
     filename = f"{str(a)}.png"
-    path = './input/inpaint/mask_image'
-    content_byte = str.encode(file)
-    content = base64.b64decode(content_byte)
-    with open(os.path.join(path, filename), "w+b") as fp:
-        fp.write(content)
+    path = './input/inpaint/base_image'
+    print('activate2')
+    download = request.urlopen(file).read() # <-- 오류코드 외부URL은 s괜찮은데 내부로 요청하면 멈춤
+    print('activate3')
+    
+    with open(os.path.join(path, filename), 'w+b') as fp:
+        fp.write(download)
+
     return f'{path}/{filename}'
 
 
@@ -40,10 +47,13 @@ async def save_mask_img(file):
     a = time.strftime("%Y-%m-%d-%H-%M-%S")
     filename = f"{str(a)}.png"
     path = './input/inpaint/mask_image'
+
     content_byte = str.encode(file)
     content = base64.b64decode(content_byte)
+
     with open(os.path.join(path, filename), "w+b") as fp:
         fp.write(content)
+
     return f'{path}/{filename}'
 
 
@@ -53,7 +63,7 @@ async def get_prompt_img2img(keyword: str = Form(), W: str = Form(), H: str = Fo
                             samples: str = Form(), base_img: str = Form()):
     
     #TODO
-    # 코드 돌리고 나온 이미지 경로 리스트 b 변수에 저장
+    #코드 돌리고 나온 이미지 경로 리스트 b 변수에 저장
     #global b
 
     prompt = translator.translate(keyword, dest='en').text
@@ -66,25 +76,20 @@ async def get_prompt_img2img(keyword: str = Form(), W: str = Form(), H: str = Fo
 
     #b = img2img.image_list
     
-    return 'http://0.0.0.0:3333/generated_img2img'
+    return 'http://192.168.0.113:3333/generated_img2img'
 
 
 @prompt.post('/inpaint/keyword', status_code=status.HTTP_200_OK)
 async def get_prompt(base_img: str = Form(), mask_img: str = Form(), 
                      keyword: str = Form(), steps: str = Form(), style : str = Form()):
     global inpainted_image
-    # base_img = image url
-    # python request lib
-
 
     prompt = translator.translate(keyword, dest='en').text
-    with request.urlopen(base_img) as img:
-        base_img = base64.b64encode(img.read()).decode('utf-8')
 
     base_path = await save_base_img(base_img)
     mask_path = await save_mask_img(mask_img)
 
-    inpainted_image = inpaint.main(style=style, base_path=base_path, mask_path=mask_path, prompt=prompt)
+    # inpainted_image = inpaint.main(style=style, base_path=base_path, mask_path=mask_path, prompt=prompt)
     
     return 'http://192.168.0.113:3333/generated_inpaint'
 
@@ -103,15 +108,15 @@ async def get_image_inpainted(id : int):
         return
     
 
-@prompt.get('/generated_inpaint')
-async def get_image_img2img():
-    return FileResponse(inpainted_image)
-
-
 @prompt.get('/generated_img2img')
 async def get_image():
-    url = ['http://0.0.0.0:3333/generated_img2img/1', 
-           'http://0.0.0.0:3333/generated_img2img/2', 
-           'http://0.0.0.0:3333/generated_img2img/3', 
-           'http://0.0.0.0:3333/generated_img2img/4']
+    url = ['http://192.168.0.113:3333/generated_img2img/1', 
+           'http://192.168.0.113:3333/generated_img2img/2', 
+           'http://192.168.0.113:3333/generated_img2img/3', 
+           'http://192.168.0.113:3333/generated_img2img/4']
     return url
+    
+
+# @prompt.get('/generated_inpaint')
+# async def get_image_img2img():
+#     return FileResponse(inpainted_image)
