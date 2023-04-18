d38779bf90f88b7de470c366fa21b8de3eb3e401
repairedserver2 <dnjfdid14@naver.com>
2023-04-18 20:22:20 +0900

    middleware

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from fastapi import FastAPI
 import uvicorn
 from app import api_router
+from starlette.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title='Chromatica APP', version='1.0.0')
 app.include_router(api_router)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get('/')
 async def test():
